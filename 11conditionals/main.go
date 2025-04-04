3501e011bb45abba75c1d72b62efab08dddbf4df
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-04-04 23:38:00 +0530

    feat: Update conditional logic with switch statement

--- 11conditionals/main.go
+++ 11conditionals/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	fmt.Println("Understanding conditional statements in Golang")
 
-	// If else statements
-
+	//<---------------------- If else statements ----------------------->
 	loginCount := 23
 	var result string
 
@@ -34,4 +37,26 @@ func main() {
 		fmt.Println("Number is greater than or equal to 10")
 	}
 
+	//<---------------------- Switch statement ----------------------->
+	source := rand.NewSource(time.Now().UnixNano()) // seed with a specific value
+	r := rand.New(source)
+	diceNumber := r.Intn(6) + 1
+
+	fmt.Println("Value of dice is:", diceNumber)
+	switch diceNumber {
+	case 1:
+		fmt.Println("Dice value is 1 and you can open")
+	case 2:
+		fmt.Println("You can move 2 spot")
+	case 3:
+		fmt.Println("You can move to 3 spot")
+	case 4:
+		fmt.Println("You can move to 4 spot")
+	case 5:
+		fmt.Println("You can move to 5 spot")
+	case 6:
+		fmt.Println("You can move to 6 spot and roll dice again")
+	default:
+		fmt.Println("Invalid dice roll")
+	}
 }
