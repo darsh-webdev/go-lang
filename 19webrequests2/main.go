1cfb3c82d9d6da3992036979e255f480b1f27526
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-04-13 07:58:57 +0530

    feat: Implement get request to custom server in Go

---
+++ 19webrequests2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Handling web requests to local server in Golang")
+
+	PerformGetRequest()
+}
+
+func PerformGetRequest() {
+	const myUrl = "http://localhost:3000/get"
+
+	response, err := http.Get(myUrl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Status code: ", response.StatusCode)
+	fmt.Println("Content length is: ", response.ContentLength)
+
+	var responseString strings.Builder
+	content, _ := io.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+
+	fmt.Println("ByteCount is: ", byteCount)
+	fmt.Println("Content is: ", responseString.String())
+}
