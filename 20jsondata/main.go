2c2b2998104623402fdf0d6b1f56a09cc3bfd4a9
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-04-17 21:47:09 +0530

    add: Demonstrate JSON data decoding in Go

--- 20jsondata/main.go
+++ 20jsondata/main.go
@@ -16,6 +16,7 @@ type course struct {
 func main() {
 	fmt.Println("Understanding more about JSON data in Golang")
 	EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -35,4 +36,37 @@ func EncodeJson() {
 
 	fmt.Printf("%s\n", finalJson)
 
+}
+
+func DecodeJson() {
+	demoJsonData := []byte(`
+	{
+        "courseName": "Go Programming",
+        "Price": 299,
+        "website": "Udemy",
+        "tags": ["high-level","backend"]
+    }
+	`)
+
+	var myCourse course
+
+	checkValid := json.Valid(demoJsonData)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(demoJsonData, &myCourse)
+		fmt.Printf("%#v\n", myCourse)
+	} else {
+		fmt.Println("JSON was invalid")
+	}
+
+	// Some cases where you just want to add data to key value pairs
+
+	var myOnlineCourse map[string]interface{}
+	json.Unmarshal(demoJsonData, &myOnlineCourse)
+	fmt.Printf("%#v\n", myOnlineCourse)
+
+	for k, v := range myOnlineCourse {
+		fmt.Printf("Key is %v and value is %v and Type is: %T\n", k, v, v)
+	}
 }
