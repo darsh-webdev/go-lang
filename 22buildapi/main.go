0cee54fb3d295b9607632542336bf201526d13f3
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-04-20 01:02:44 +0530

    add: Implement getCourseById API function in Go

--- 22buildapi/main.go
+++ 22buildapi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Model for course - file
@@ -43,4 +45,24 @@ func getAllCourses(w http.ResponseWriter, r *http.Response) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+// get course based on id
+func getCourseById(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get course by id")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop through courses, find matching id and return the response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No Course found with given id")
+	return
 }
