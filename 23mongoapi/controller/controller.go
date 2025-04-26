ddadb8a2ad240b63ef754f8e47fb0c3e53e51307
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-04-26 18:16:02 +0530

    feat: Add MongoDB helpers for CRUD operations

--- 23mongoapi/controller/controller.go
+++ 23mongoapi/controller/controller.go
@@ -1,13 +1,17 @@
 package controller
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	"github.com/darsh-webdev/mongoapi/model"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
+// Change username and password
 const connectionString = "mongodb+srv://<db_username>:<db_password>@cluster0.n44sg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const dbName = "netflix"
 const colName = "watchlist"
@@ -34,4 +38,65 @@ func init() {
 
 	// collection instance
 	fmt.Println("Collection instance is ready")
+}
+
+// MongoDB helpers
+
+// insert a record
+func insertMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted one movie in DB with id: ", inserted.InsertedID)
+}
+
+// update a record
+func updateMovie(movieId string) {
+	id, err := bson.ObjectIDFromHex(movieId)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Modified Count: ", result.MatchedCount)
+}
+
+// delete a record
+func deleteMovie(movieId string) {
+	id, err := bson.ObjectIDFromHex(movieId)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Movie got deleted with delete count: ", deleteCount)
+}
+
+// delete all records from DB
+func deleteAllMovies() {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of movies deleted: ", deleteResult.DeletedCount)
 }
